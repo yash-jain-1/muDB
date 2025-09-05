992ebfce0e57249355527e02dfee108c1cd18fe0
Yash Jain <114145222+yash-jain-1@users.noreply.github.com>
2025-09-05 09:40:28 +0000

    Implement list operations in DB: add lpush, rpush, and lrange methods

--- src/storage/db.rs
+++ src/storage/db.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, VecDeque},
     sync::{Arc, RwLock},
 };
 
@@ -30,11 +30,12 @@ pub struct Entry {
 }
 
 /// The `Value` enum allows for storing various types of data associated with a key.
-/// Currently, it supports only String data type. But it can be expanded in the future
-/// to support more data types as needed (like List, Hash etc).
+/// Currently, it supports only String and List data type. But it can be expanded in the future
+/// to support more data types as needed (like Hash, SortedSet etc).
 #[derive(Debug, Clone)]
 pub enum Value {
     String(String),
+    List(VecDeque<String>)
 }
 
 impl Storage {
@@ -82,8 +83,10 @@ impl DB {
             None => return Ok(None),
         };
 
-        let Value::String(s) = &entry.value;
-        return Ok(Some(s.to_string()));
+        match &entry.value {
+            Value::String(s) => Ok(Some(s.to_string())),
+            _ => Err(DBError::WrongType),
+        }
     }
 
     /// Set a string value against a key.
@@ -122,6 +125,183 @@ impl DB {
         return Ok(());
     }
 
+    /// Add new elements to the head of a list.
+    /// If the key is not present in the DB, and empty list is initialized
+    /// against the key before adding the elements to the head.
+    ///
+    /// # Arguments
+    ///
+    /// * `k` - The key on which list is stored.
+    ///
+    /// * `v` - The values to be added to the head of the list.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(())` - If values are added successfully to the head of the list.
+    /// * `Err(DBError)` - if key already exists and has non-list data.
+    pub fn lpush(&self, k: String, v: Vec<String>) -> Result<usize, DBError> {
+        let mut data = match self.data.write() {
+            Ok(data) => data,
+            Err(e) => return Err(DBError::Other(format!("{}", e))),
+        };
+
+        let entry = match data.get_mut(k.as_str()) {
+            Some(entry) => Some(entry),
+            None => None,
+        };
+
+        match entry {
+            Some(e) => {
+                let val = &mut e.value;
+                match val {
+                    Value::List(l) => {
+                        for each in v.iter().cloned() {
+                            l.push_front(each);
+                        }
+                        Ok(l.len())
+                    }
+                    _ => Err(DBError::WrongType),
+                }
+            }
+            None => {
+                let list = VecDeque::from(v);
+                let l_len = list.len();
+                data.insert(k.to_string(), Entry::new(Value::List(list)));
+
+                Ok(l_len)
+            }
+        }
+    }
+
+    /// Adds new elements to the tail of a list.
+    /// If the key is not present in the DB, and empty list is initialized
+    /// against the key before adding the elements to the tail.
+    ///
+    /// # Arguments
+    ///
+    /// * `k` - The key on which list is stored.
+    ///
+    /// * `v` - The values to be added to the tail of the list.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(())` - If value are added successfully to the tail of the list.
+    /// * `Err(DBError)` - if key already exists and has non-list data.
+    pub fn rpush(&self, k: String, v: Vec<String>) -> Result<usize, DBError> {
+        let mut data = match self.data.write() {
+            Ok(data) => data,
+            Err(e) => return Err(DBError::Other(format!("{}", e))),
+        };
+
+        let entry = match data.get_mut(k.as_str()) {
+            Some(entry) => Some(entry),
+            None => None,
+        };
+
+        match entry {
+            Some(e) => {
+                let val = &mut e.value;
+                match val {
+                    Value::List(l) => {
+                        for each in v.iter().cloned() {
+                            l.push_back(each);
+                        }
+                        Ok(l.len())
+                    }
+                    _ => Err(DBError::WrongType),
+                }
+            }
+            None => {
+                let list = VecDeque::from(v);
+                let l_len = list.len();
+                data.insert(k.to_string(), Entry::new(Value::List(list)));
+
+                Ok(l_len)
+            }
+        }
+    }
+
+    /// Returns the specified number of elements of the list stored at key, based on the start and stop indices.
+    /// These offsets can also be negative numbers indicating offsets starting at the end of the list.
+    /// For example, -1 is the last element of the list, -2 the penultimate, and so on.
+    /// Please note that the item at stop index is also included in the result.
+    ///
+    /// If the specified key is not found, an empty list is returned.
+    ///
+    /// # Arguments
+    ///
+    /// * `k` - The key on which list is stored.
+    ///
+    /// * `start_idx` - The start index.
+    ///
+    /// * `stop_idx` - The end index.
+    ///
+    /// # Returns
+    ///
+    /// * `Ok(Vec<String>)` - If values are retrieved successfully from the list.
+    /// * `Err(DBError)` - if key already exists and has non-list data.
+    pub fn lrange(&self, k: String, start_idx: i64, stop_idx: i64) -> Result<Vec<String>, DBError> {
+        let data = match self.data.read() {
+            Ok(data) => data,
+            Err(e) => return Err(DBError::Other(format!("{}", e))),
+        };
+
+        let entry = match data.get(k.as_str()) {
+            Some(entry) => entry,
+            None => return Ok(vec![]),
+        };
+
+        match &entry.value {
+            Value::List(l) => {
+                let l_len = l.len() as i64;
+                let (rounded_start_idx, rounded_stop_idx) =
+                    Self::round_list_indices(l_len, start_idx, stop_idx);
+                Ok(l.range(rounded_start_idx..rounded_stop_idx)
+                    .cloned()
+                    .collect())
+            }
+            _ => Err(DBError::WrongType),
+        }
+    }
+
+    /// Round index to 0, if the given index value is less than zero.
+    /// Round index to list length, if the given index value is greater then the list length.
+    fn round_list_index(list_len: i64, idx: i64) -> usize {
+        if idx < 0 {
+            let idx = list_len - idx.abs();
+            if idx < 0 {
+                return 0;
+            } else {
+                return idx as usize;
+            }
+        }
+
+        if idx >= list_len {
+            return (list_len - 1) as usize;
+        }
+
+        return idx as usize;
+    }
+
+    /// Round the start and stop indices using `Self::round_list_index` method and return them as
+    /// a tuple.
+    /// Special condition: If stop index is lower than start index, return (0, 0).
+    fn round_list_indices(list_len: i64, start_idx: i64, stop_idx: i64) -> (usize, usize) {
+        if stop_idx < start_idx {
+            return (0, 0);
+        }
+
+        let rounded_start_idx = Self::round_list_index(list_len, start_idx);
+        let rounded_stop_idx = Self::round_list_index(list_len, stop_idx);
+
+        if rounded_start_idx < rounded_stop_idx {
+            (rounded_start_idx, rounded_stop_idx + 1)
+        } else if rounded_stop_idx < rounded_start_idx {
+            (0, 0)
+        } else {
+            (rounded_start_idx, rounded_start_idx + 1)
+    }
+        }
 }
 
 impl Entry {
